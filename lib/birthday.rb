368e56021ad55f9bf731c4df78c0352eb51239df
github username <github email address>
2019-05-29 17:14:25 -0700

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,14 +5,14 @@
 # }
 
 def happy_birthday(birthday_kids)
-birthday_kids.each do |kids_name, age|
-  puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  birthday_kids.each do |name, age|
+    puts "Happy Birthday #{name}! You are now #{age} years old!"
   end
 end
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |name, age|
-    if age <= 12
+    if age < 12
       puts "Happy Birthday #{name}! You are now #{age} years old!"
     else
       puts "You are too old for this."
--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -13,5 +13,17 @@ def select_winner(passengers)
       winner = name
     end
   end
+
   winner
 end
+
+# def select_winner(passengers)
+#   winner = ""
+#   passengers.each do |suite, name|
+#     if suite == :suite_a && name.start_with?("A")
+#       winner = name
+#     end
+#   end
+#
+#   winner
+# end
